e658b96b5d9da3fd6b869cd53e0d2cff59915ee9
coconut32 <coconut_liu@126.com>
2016-10-29 08:45:05 +0800

    Create LA.c

---
+++ LA.c
@@ -0,0 +1,10 @@
+Problems and theorems in linear algebra
+
+Techniques
+T1.mathematical induction
+T2.pretend variable as constant
+T3.substitute and compute
+
+1.1 (Cramer rule) 
+Pf1.do T1. the base step is trivial, for the induction step, use T2.
+Pf2.do T3 to rhs.
