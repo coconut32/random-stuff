7c82bfa46f5b4aedc3f6a2b9d9c32e7ec1bbdc6f
coconut32 <coconut_liu@126.com>
2016-10-29 09:17:56 +0800

    Update LA.c

--- LA.c
+++ LA.c
@@ -4,7 +4,14 @@ Techniques
 T1.mathematical induction
 T2.pretend variable as constant
 T3.substitute and compute
+T4.view the left action by a matrix as row operation
+T5.work on the level of block
+T6.decomposition into products (maybe by guesswork)
 
 1.1 (Cramer rule) 
 Pf1.do T1. the base step is trivial, for the induction step, use T2.
 Pf2.do T3 to rhs.
+
+1.7 (generalization of row operation)
+Pf1.use T4.
+Pf2.use T5 combined with T6.
