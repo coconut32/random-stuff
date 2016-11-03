6f9dbb4029dce2b34a4106cbce46c91dcd6bf486
coconut32 <coconut_liu@126.com>
2016-11-04 02:29:14 +0800

    Update LA.c

--- LA.c
+++ LA.c
@@ -7,15 +7,21 @@ T3.substitute and compute
 T4.view the left action by a matrix as row operation
 T5.work on the level of block
 T6.decomposition into products (maybe by guesswork)
+T7.Cauchy-Binet
 
 1.1 (Cramer rule) 
-Pf1.do T1. the base step is trivial, for the induction step, use T2.
-Pf2.do T3 to rhs.
+P1.do T1. the base step is trivial, for the induction step, use T2.
+P2.do T3 to rhs.
 
 1.7 (generalization of row operation)
-Pf1.use T4.
-Pf2.use T5 combined with T6.
+P1.use T4.
+P2.use T5 combined with T6.
 
 2.2
-Pf1.immediate by contradiction.
-Pf2.compute to construct an explicit linear relationship.
+P1.immediate by contradiction.
+P2.compute to construct an explicit linear relationship.
+
+2.3 (Cauchy-Binet)
+This result does not admit a simpler proof so itself will be added to the list of tools.
+We prove this for the smallest non-trivial case.
+the proof is at Cauchy-Binet.jpg.
